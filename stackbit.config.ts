4a69ea2fcd8f0978b62e8c740f3c8f75b9a91d46
Maxime Grauliere <ton.email@domaine.tld>
2025-11-11 15:25:20 -0500

    chore(editor): declare content source + siteMap for Netlify Visual Editor

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,58 +1,18 @@
-import { defineStackbitConfig, DocumentStringLikeFieldNonLocalized, SiteMapEntry } from '@stackbit/types';
-import { GitContentSource } from '@stackbit/cms-git';
-import { allModels } from 'sources/local/models';
+﻿import { defineStackbitConfig } from "@netlify/visual-editor";
 
-const gitContentSource = new GitContentSource({
-    rootPath: __dirname,
-    contentDirs: ['content'],
-    models: Object.values(allModels),
-    assetsConfig: {
-        referenceType: 'static',
-        staticDir: 'public',
-        uploadDir: 'images',
-        publicPath: '/'
+export default defineStackbitConfig({
+  stackbitVersion: "~0.6.0",
+  contentSources: [
+    {
+      type: "filesystem",
+      name: "Git CMS",
+      path: "content",
+      models: "*"
     }
+  ],
+  siteMap: () => ([
+    { model: "PageLayout", urlPath: "/" },
+    { model: "PageLayout", urlPath: "/{slug}" },
+    { model: "PostLayout", urlPath: "/blog/{slug}" }
+  ])
 });
-
-export const config = defineStackbitConfig({
-    stackbitVersion: '~0.7.0',
-    ssgName: 'nextjs',
-    nodeVersion: '18',
-    styleObjectModelName: 'ThemeStyle',
-    contentSources: [gitContentSource],
-    presetSource: {
-        type: 'files',
-        presetDirs: ['sources/local/presets']
-    },
-    siteMap: ({ documents, models }): SiteMapEntry[] => {
-        const pageModels = models.filter((model) => model.type === 'page').map((model) => model.name);
-        return documents
-            .filter((document) => pageModels.includes(document.modelName))
-            .map((document) => {
-                let slug = (document.fields.slug as DocumentStringLikeFieldNonLocalized)?.value;
-                if (!slug) return null;
-                /* Remove the leading slash in order to generate correct urlPath
-                regardless of whether the slug is '/', 'slug' or '/slug' */
-                slug = slug.replace(/^\/+/, '');
-                switch (document.modelName) {
-                    case 'PostFeedLayout':
-                        return {
-                            urlPath: '/blog',
-                            document: document
-                        };
-                    case 'PostLayout':
-                        return {
-                            urlPath: `/blog/${slug}`,
-                            document: document
-                        };
-                    default:
-                        return {
-                            urlPath: `/${slug}`,
-                            document: document
-                        };
-                }
-            });
-    }
-});
-
-export default config;
